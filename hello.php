64e7a10c6e324363fba4f2ac7a2861859c13fdf3
Denys Pylypenko <denyspylypenko1986@gmail.com>
2021-05-12 14:22:40 +0300

    readme

---
+++ hello.php
@@ -0,0 +1,14 @@
+<! DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Начинаем работу с PHP</title>
+</head>
+<body>
+<?php
+// традиционное приветствие.
+echo '<h1>Привет, мир!</h1>';
+
+?>
+
+</body>
---
+++ variable.php
@@ -0,0 +1,20 @@
+<! DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Начинаем работу с PHP</title>
+</head>
+<body>
+<?php
+
+$body_temp = 36.6;
+echo $body_temp;
+echo "<p> Температура тела составляет $body_temp градусов цельсия";
+
+$body_temp = 97.88;
+echo " ($body_temp градусов Фаренгейта)</p>"
+
+?>
+
+</body>
+</html>
