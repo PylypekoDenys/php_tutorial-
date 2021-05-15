3accd7974f1d8eeb090d8f5ac0e3515cc5d5f058
Denys Pylypenko <denyspylypenko1986@gmail.com>
2021-05-15 16:39:55 +0300

    readme for me

---
+++ breakcontinue.php
@@ -0,0 +1,31 @@
+<! DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Начинаем работу с PHP</title>
+</head>
+<body>
+<?php for ($i = 1; $i < 4; $i++)
+{
+    for ($j = 1; $j < 4; $j++)
+    {
+        if ($i == 1 && $j == 1)
+        {
+            echo "Продолжить внутренний цикл i = $i и j = $j<br>";
+            continue;
+        }
+        if ($i == 2 && $j == 1)
+        {
+            echo "Прервать внутренний цикл, если i = $i и j = $j<br>";
+            break;
+        }
+        echo "Выполнено i = $i и j = $j <br>";
+
+    }
+}
+
+
+
+    ?>
+</body>
+</html>
